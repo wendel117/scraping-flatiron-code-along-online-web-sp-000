d619aeda03685e2c29d79bf0d8e8c7e0fd05b2c6
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-27 17:46:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -21,8 +21,5 @@ class Scraper
 
   def get_courses
   end
-  
+
 end
-
-
-
